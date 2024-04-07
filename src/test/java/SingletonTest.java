9c5f7a4a01d752f7c092e014ea1512a0766aca4d
karae <karaev.den@gmail.com>
2024-04-07 22:34:10 +0300

    ll

--- src/test/java/SingletonTest.java
+++ src/test/java/SingletonTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SingletonTest {
     @Test
     public void returnInstance() {
-        Connection connection1 = connection1.getInstance();
-        Connection connection2 = connection1.getInstance();
+        Connection connection1 = Connection.getInstance()
+        Connection connection2 = Connection.getInstance();
         assertEquals(connection1, connection2);
     }
 }
