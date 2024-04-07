526918557d0b4be8700b71994803f9f79d1449b7
karae <karaev.den@gmail.com>
2024-04-07 23:13:43 +0300

    ll

---
+++ src/test/java/AdapterTest.java
@@ -0,0 +1,9 @@
+import org.example.lab2.Adapter.RoundHole;
+import org.example.lab2.Adapter.SquareStick;
+import org.example.lab2.Adapter.SquareStickAdapter;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class AdapterTest {
+
+}
