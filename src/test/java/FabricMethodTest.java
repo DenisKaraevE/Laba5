5c99be02a2f1779108724b0bfd9cff89cc9f1452
karae <karaev.den@gmail.com>
2024-04-07 22:55:37 +0300

    ll

--- src/test/java/FabricMethodTest.java
+++ src/test/java/FabricMethodTest.java
@@ -1,2 +1,12 @@
+import org.example.lab1.AbstractFabric.Configuration;
+import org.junit.jupiter.api.Test;
+import static org.example.lab1.FactoryMethod.Configuration.MOB;
+import static org.example.lab1.FactoryMethod.Configuration.WEB;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FabricMethodTest {
+    @Test
+    public void testMob(){
+        Configuration configuration = Configuration.initMob();
+        assertEquals(MOB, configuration.getPlatform());
+    }
 }
