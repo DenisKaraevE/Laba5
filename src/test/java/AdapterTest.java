73a64731f3b21ab2821cba6d39f7bcde790d3478
karae <karaev.den@gmail.com>
2024-04-07 23:15:24 +0300

    ll

--- src/test/java/AdapterTest.java
+++ src/test/java/AdapterTest.java
@@ -5,5 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AdapterTest {
-
+    @Test
+    public void Test1(){
+        RoundHole hole = new RoundHole(5); // radius = 5
+        SquareStick stick = new SquareStick(5); // width = 5 => diagonal = 7.07 => radius = 3.535
+        SquareStickAdapter adapter = new SquareStickAdapter(stick);
+        boolean result = hole.fits(adapter);
+        assertTrue(result);
+    }
+    @Test
+    public void Test2(){
+        RoundHole hole = new RoundHole(5); // radius = 5
+        SquareStick stick = new SquareStick(10); // width = 10 => diagonal = 14.14 => radius = 7.07
+        SquareStickAdapter adapter = new SquareStickAdapter(stick);
+        boolean result = hole.fits(adapter);
+        assertFalse(result);
+    }
 }
