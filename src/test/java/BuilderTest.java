ecd5cbde198f7450d5aee47766b407010d3a63e3
karae <karaev.den@gmail.com>
2024-04-07 23:02:16 +0300

    ll

--- src/test/java/BuilderTest.java
+++ src/test/java/BuilderTest.java
@@ -1,2 +1,34 @@
+import org.example.lab1.Builder.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BuilderTest {
+    CarBuilder builder = new CarBuilder();
+    CarType expectedType = CarType.SPORTS_CAR;
+    int expectedSeats = 2;
+    Engine expectedEngine = new Engine(3.0, 0);
+    Transmission expectedTransmission = Transmission.SEMI_AUTOMATIC;
+    TripComputer expectedTripComputer = new TripComputer();
+    GPSNavigator expectedGPSNavigator = new GPSNavigator();
+
+        builder.setCarType(expectedType);
+        builder.setSeats(expectedSeats);
+        builder.setEngine(expectedEngine);
+        builder.setTransmission(expectedTransmission);
+        builder.setTripComputer(expectedTripComputer);
+        builder.setGPSNavigator(expectedGPSNavigator);
+    Car car = builder.getResult();
+
+    assertEquals(expectedType, car.getCarType());
+
+    assertEquals(expectedSeats, car.getSeats());
+
+    assertEquals(expectedEngine, car.getEngine());
+
+    assertEquals(expectedTransmission, car.getTransmission());
+
+    assertEquals(expectedTripComputer, car.getTripComputer());
+
+    assertEquals(expectedGPSNavigator, car.getGpsNavigator());
 }
