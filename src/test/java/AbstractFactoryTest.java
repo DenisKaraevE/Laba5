3277d75dab3e4a16ea9bf175810fe8b15711cd01
karae <karaev.den@gmail.com>
2024-04-07 23:17:32 +0300

    ll

---
+++ src/test/java/AbstractFactoryTest.java
@@ -0,0 +1,12 @@
+import org.example.lab1.AbstractFabric.Configuration;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class AbstractFactoryTest {
+    public final static int WEB = 1;
+    public final static int MOB = 2;
+    @Test
+    public void testMob(){
+        Configuration configuration =  Configuration.initMob();
+        assertEquals(MOB, configuration.getPlatform());
+    }
+}
