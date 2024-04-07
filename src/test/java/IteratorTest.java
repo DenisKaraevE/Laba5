775eab923b8389a597cbb691d04c2edf58802423
karae <karaev.den@gmail.com>
2024-04-07 22:53:22 +0300

    ll

--- src/test/java/IteratorTest.java
+++ src/test/java/IteratorTest.java
@@ -2,13 +2,14 @@ import org.example.lab3.Iterator.ItemType;
 import org.example.lab3.Iterator.TreasureChest;
 import org.example.lab3.Iterator.TreasureChestItemIterator;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IteratorTest {
     @Test
-    public void test(){
+    public void test() {
         TreasureChest chest = new TreasureChest();
-        TreasureChestItemIterator chestIterator = new
-                TreasureChestItemIterator (chest, ItemType.POTION);
+        TreasureChestItemIterator chestIterator = new TreasureChestItemIterator(chest, ItemType.POTION);
         assertEquals(chestIterator.next().toString(), "Potion of courage");
         assertEquals(chestIterator.next().toString(), "Potion of wisdom");
         assertEquals(chestIterator.next().toString(), "Potion of blood");
