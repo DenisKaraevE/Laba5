3e79d4b9ab62544e0015490a8cfa0427669a94f4
karae <karaev.den@gmail.com>
2024-04-07 22:41:04 +0300

    ll

--- src/test/java/StrategyTest.java
+++ src/test/java/StrategyTest.java
@@ -1,21 +1,24 @@
 import org.example.lab3.Strategy.*;
 import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StrategyTest {
     @Test
-    public void returnCorrectString(){
+    public void returnCorrectString() {
         List<String> strings = new ArrayList<>();
         strings.add("корова");
         strings.add("собака");
         strings.add("мышь");
         strings.add("кошка");
-Strategy strategy = new CommaSeparatedStrategy();
-StrategyService service =new StrategyServiceImpl(strategy);
-assertEquals("корова,собака,мышь,кошка", service.exec(strings));
-strategy = new SemicolonSeparatedStrategy();
-service.setStrategy(strategy);
-assertEquals("корова,собака,мышь,кошка", service.exec(strings));
+        Strategy strategy = new CommaSeparatedStrategy();
+        StrategyService service = new StrategyServiceImpl(strategy);
+        assertEquals("корова,собака,мышь,кошка", service.exec(strings));
+        strategy = new SemicolonSeparatedStrategy();
+        service.setStrategy(strategy);
+        assertEquals("корова,собака,мышь,кошка", service.exec(strings));
     }
 }
